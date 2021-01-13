e83406ea4cc1824d3ba58d9e438ae7cb015ffe75
mahdi aghaei <mhd.agi01@gmail.com>
2021-01-13 15:23:13 +0330

    finished demo and practice

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 const { Engine, World, Runner, Render, Bodies, Mouse, MouseConstraint } = Matter;
 
+const width = 800;
+const height = 600;
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
     element: document.body,
     engine: engine,
     option: {
-        width: 800,
-        height:600
+        width,
+        height
     }
 });
 
@@ -24,4 +26,7 @@ const walls = [
     Bodies.rectangle(800, 300, 40, 600, {isStatic:true})
 ];
 World.add(world, walls)
-World.add(world, Bodies.rectangle(200,200,50,50))
+
+for(let i =0; i < 20; i++){
+    World.add(world, Bodies.rectangle(Math.random() * 800, Math.random() * 600 , 50, 50))
+}

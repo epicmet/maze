861a42c92bb8e28f6f2a7040122f118ca324d9c8
mahdi aghaei <mhd.agi01@gmail.com>
2021-01-19 23:29:12 +0330

    complete maze generation

--- src/app.js
+++ src/app.js
@@ -25,10 +25,74 @@ const walls = [
 ];
 World.add(world, walls)
 
-for(let i =0; i < 20; i++){
-    World.add(world, Bodies.rectangle(Math.random() * 800, Math.random() * 600 , 50, 50) ,{
-        render: {
-            fillStyle : 'red'
+// Maze generation
+const cells = 3;
+
+const grid = Array(cells).fill(null).map(() => Array(cells).fill(false));
+const verticals = Array(cells).fill(null).map(() => Array(cells -1).fill(false));
+const horizontals = Array (cells -1).fill(null).map(() => Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const shuffle = (arr) => {
+    let counter = arr.length;
+    
+    while(counter > 0){
+        let index = Math.floor(Math.random() * counter);
+
+        counter--;
+
+        let temp = arr[counter];
+        arr[counter] = arr[index];
+        arr[index] = temp;
+    }
+
+    return arr;
+};
+
+const stepThroughCell = (row, column) => {
+    //check if cell is visited
+    if(grid[row][column]) return;
+    
+    //mark cell as visited
+    grid[row][column] = true;
+
+    //random order neighbor coordinates
+    const neighbors = shuffle([
+        [row - 1, column, 'up'],
+        [row, column + 1, 'right'],
+        [row + 1, column, 'down'],
+        [row, column - 1, 'left']
+    ]);
+
+    //for each neighbor...
+    for(neighbor of neighbors){
+        [nextRow, nextColumn, direction] = neighbor;
+        //see if neighbor is out of bound
+        if(nextRow < 0 || nextRow >=cells || nextColumn < 0 || nextColumn >=cells){
+            continue;
         }
-    })
-}
+
+        //see if neighbor is visited befor
+        if(grid[nextRow][nextColumn]){
+            continue;
+        }
+
+        //Remove a wall
+        if(direction === 'left'){
+            verticals[row][column - 1] = true;
+        }else if(direction === 'right'){
+            verticals[row][column] = true;
+        }else if(direction === 'up'){
+            horizontals[row - 1][column] = true;
+        }else if(direction === 'down'){
+            horizontals[row][column] = true;
+        }
+
+        stepThroughCell(nextRow, nextColumn);
+    }
+
+};
+
+stepThroughCell(startRow, startColumn);

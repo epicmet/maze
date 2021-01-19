7e24b6555ec0f225b5e4ef186a0a0029cbaf4241
mahdi aghaei <mhd.agi01@gmail.com>
2021-01-19 18:32:16 +0330

    made walls alignable

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
-const { Engine, World, Runner, Render, Bodies, Mouse, MouseConstraint } = Matter;
+const { Engine, World, Runner, Render, Bodies, } = Matter;
 
-const width = 800;
+const width = 600;
 const height = 600;
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
     element: document.body,
     engine: engine,
-    option: {
+    options: {
+        wireframes: true,
         width,
         height
     }
@@ -15,18 +16,19 @@ const render = Render.create({
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
-World.add(world, MouseConstraint.create(engine , {
-    mouse: Mouse.create(render.canvas)
-}));
 
 const walls = [
-    Bodies.rectangle(400, 0, 800, 40, {isStatic:true}),
-    Bodies.rectangle(400, 600, 800, 40, {isStatic:true}),
-    Bodies.rectangle(0, 300, 40, 600, {isStatic:true}),
-    Bodies.rectangle(800, 300, 40, 600, {isStatic:true})
+    Bodies.rectangle(width / 2, 0, width, 40, {isStatic:true}),
+    Bodies.rectangle(width / 2, height, width, 40, {isStatic:true}),
+    Bodies.rectangle(0, height / 2, 40, height, {isStatic:true}),
+    Bodies.rectangle(width, height / 2, 40, height, {isStatic:true})
 ];
 World.add(world, walls)
 
 for(let i =0; i < 20; i++){
-    World.add(world, Bodies.rectangle(Math.random() * 800, Math.random() * 600 , 50, 50))
+    World.add(world, Bodies.rectangle(Math.random() * 800, Math.random() * 600 , 50, 50) ,{
+        render: {
+            fillStyle : 'red'
+        }
+    })
 }
